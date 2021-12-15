0c68a95a95b32e4427839c2d93dd488e867044a8
frans2001 <fransnino.hamtig@evsu.edu.ph>
2021-12-15 14:18:36 +0800

    Retaking Exam

--- annual_salary.java
+++ annual_salary.java
@@ -21,31 +21,64 @@ class fff {
       annual_tax_deduction = tax_deduction;
       System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
     }
-    else if ((annual_salary >= 400000) && (annual_salary <= 800000))  {
-      tax = annual_salary-30000-400000;
-      tax_deduction = tax * .25 + 30000;
-      annual_tax_deduction = tax_deduction;
-      System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
-    }
-    else if ((annual_salary >= 800000) && (annual_salary <= 2000000)) {
-      tax = annual_salary-130000-800000;
-      tax_deduction = tax * .30 + 130000;
-      annual_tax_deduction = tax_deduction;
-      System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
-    }
-    else if ((annual_salary >= 2000000) && (annual_salary <= 8000000))  {
-      tax = annual_salary-490000-2000000;
-      tax_deduction = tax * .32 + 490000;
-      annual_tax_deduction = tax_deduction;
-      System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
-    }
-    else if (annual_salary >= 8000000)  {
-      tax = annual_salary-2410000-8000000;
-      tax_deduction = tax * .35 + 2410000;
-      annual_tax_deduction = tax_deduction;
-      System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
-    }
-
+    while (annual_salary > 400000 && annual_salary <= 800000){
+       tax = annual_salary - 30000;
+	while(tax > 400000) {
+           annual_tax_deduction = ((tax - 400000) * 0.25) + 30000;
+	   System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+           break;
+	} 
+	if (tax < 400000) {
+		annual_tax_deduction = 30000;
+		System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+		break;
+	}
+	break;
+	}
+     while (annual_salary > 800000 && annual_salary <= 2000000){
+       tax = annual_salary - 130000;
+	while(tax > 800000) {
+           annual_tax_deduction = ((tax - 800000) * 0.30) + 130000;
+	   System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+           break;
+	} 
+	if (tax < 800000) {
+		annual_tax_deduction = 130000;
+		System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+		break;
+	}
+	break;
+	}
+      while (annual_salary > 2000000 && annual_salary <= 8000000){
+       tax = annual_salary - 130000;
+	while(tax > 2000000) {
+           annual_tax_deduction = ((tax - 2000000) * 0.32) + 490000;
+	   System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+           break;
+	} 
+	if (tax < 2000000) {
+		annual_tax_deduction = 130000;
+		System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+		break;
+	}
+	break;
+	}
+      while (annual_salary > 8000000){
+       tax = annual_salary - 2410000;
+	while(tax > 8000000) {
+           annual_tax_deduction = ((tax - 8000000) * 0.35) + 2410000;
+	   System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+           break;
+	} 
+	if (tax < 8000000) {
+		annual_tax_deduction = 2410000;
+		System.out.printf("\nyour Annual tax is : %.2f", annual_tax_deduction);
+		break;
+	}
+	break;
+	}
+   
+   
     double monthly_tax_deduction = annual_tax_deduction / 12;
     double net_pay = monthly_rate - monthly_tax_deduction;
 
